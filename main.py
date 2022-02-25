8e693498468397cde55d9c0f57ca5f321757f8d2
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2022-02-25 15:56:19 +0100

    Set greater range for parameters.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-
-
 from optimization.nsga_two import NsgaTwo
 from sim_runner.simulation_executor import  SimulationExecutor
 
 SimulationExecutor.set_cpu(2)
-SimulationExecutor.set_ram(4)
-lower_limits = [  1, 1, 0.000085415,  1]
-upper_limits = [10, 10, 0.000085417, 20]
+SimulationExecutor.set_ram(12)
+lower_limits = [  1, 1, 0.000085415,    1]
+upper_limits = [40, 20, 0.000854150, 1800]
 constraint_limit = 0.99
 NsgaTwo(lower_limits=lower_limits, upper_limits=upper_limits,
         constraint_limit=constraint_limit,
-        population_size=3, generations=1)
+        population_size=60, generations=15)
