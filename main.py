314993bd6281e427e7e6701c217db88bb5784916
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2022-02-28 12:49:12 +0100

    One-param execution.

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from sim_runner.simulation_executor import  SimulationExecutor
 
 SimulationExecutor.set_cpu(2)
 SimulationExecutor.set_ram(12)
-lower_limits = [  1, 1, 0.000085415,    1]
-upper_limits = [40, 20, 0.000854150, 1800]
+# lower_limits = [  1, 1, 0.000085415,    1]
+# upper_limits = [40, 20, 0.000854150, 1800]
+
+lower_limits = [ 9,  1, 0.00081279,  500]
+upper_limits = [10, 20, 0.00081300, 1000]
+
 constraint_limit = 0.99
 NsgaTwo(lower_limits=lower_limits, upper_limits=upper_limits,
         constraint_limit=constraint_limit,
-        population_size=60, generations=15)
+        population_size=60, generations=20)
